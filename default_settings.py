593d6558c60b13270ad2a7035c9cd449ea7c37fb
yemaozi88 <428968@gmail.com>
2021-07-30 15:25:06 +0000

    inference step is validated.

--- default_settings.py
+++ default_settings.py
@@ -3,4 +3,4 @@ import os
 repos_dir = r'/home/akikun/repos'
 repos_mlps_dir = os.path.join(repos_dir, 'mlp-singer')
 data_dir = os.path.join(repos_mlps_dir, 'data') 
-csd_dir = r'/home/akikun/db/song_corpora/CSD'
+csd_dir = r'/home/akikun/common/db/song_corpora/CSD'
